53f8b6d547eb1e495f7cc40177ae677c8138aeaf
RONIN XIII <156700077+abdennou251@users.noreply.github.com>
2025-04-20 16:49:58 +0100

    zidt tokenizer l redirection

--- minimangol/parsing/tokenize/lexer.c
+++ minimangol/parsing/tokenize/lexer.c
@@ -1,13 +1,10 @@
 #include "../../minishell.h"
 
-// int is_operator(char *token);
-
 void flag_error(char *str)
 {
     ft_putstr_fd("minishell: syntax error near unexpected token `", 2);
     ft_putstr_fd(str, 2);
     ft_putstr_fd("'\n", 1);
-    // prompt();// to do
 }
 
 void ft_free_split(char **tokens)
@@ -20,6 +17,7 @@ void ft_free_split(char **tokens)
     }
     free(tokens);
 }
+
 void check_s_char(char **token_value, int *token_type)
 {
     if (ft_strcmp(*token_value, "(") == 0)
@@ -29,6 +27,7 @@ void check_s_char(char **token_value, int *token_type)
     else
         *token_type = TOKEN_WORD;
 }
+
 void free_tokens(t_token *tokens)
 {
     t_token *current = tokens;
@@ -56,74 +55,221 @@ void create_token(t_token **head, t_token **current, char *value, int type)
     *current = new_token;
 }
 
-
-static void	add_token(t_token **head, t_token **curr, char *value, int type)
+static void add_token(t_token **head, t_token **curr, char *value, int type)
 {
-	t_token	*new;
+    t_token *new;
 
-	new = malloc(sizeof(t_token));
-	new->value = ft_strdup(value);
-	new->type = type;
-	new->next = NULL;
+    new = malloc(sizeof(t_token));
+    new->value = ft_strdup(value);
+    new->type = type;
+    new->next = NULL;
 
-	if (!*head)
-		*head = new;
-	else
-		(*curr)->next = new;
-	*curr = new;
+    if (!*head)
+        *head = new;
+    else
+        (*curr)->next = new;
+    *curr = new;
 }
 
-
-t_token	*tokenize(char *input)
+t_token *tokenize(char *input)
 {
-    t_token	*head = NULL;
-    t_token	*curr = NULL;
-    char	**tokens = ft_split(input, ' ');
-    int		i = 0;
-
-    while (tokens && tokens[i]) {
-        if (ft_strcmp(tokens[i], "|") == 0)
-            add_token(&head, &curr, tokens[i], TOKEN_PIPE);
-        else if (ft_strcmp(tokens[i], "&&") == 0)
-            add_token(&head, &curr, tokens[i], TOKEN_AND);
-        else if (ft_strcmp(tokens[i], "||") == 0)
-            add_token(&head, &curr, tokens[i], TOKEN_OR);
-        else if (ft_strcmp(tokens[i], ">") == 0)
-            add_token(&head, &curr, tokens[i], TOKEN_REDIR_OUT);
-        else if (ft_strcmp(tokens[i], "<") == 0)
-            add_token(&head, &curr, tokens[i], TOKEN_REDIR_IN);
-        else if (ft_strcmp(tokens[i], ">>") == 0)
-            add_token(&head, &curr, tokens[i], TOKEN_APPEND);
-        else if (ft_strcmp(tokens[i], "<<") == 0)
-            add_token(&head, &curr, tokens[i], TOKEN_HEREDOC);
-        else if (ft_strcmp(tokens[i], "(") == 0)
-            add_token(&head, &curr, tokens[i], TOKEN_LPAREN);
-        else if (ft_strcmp(tokens[i], ")") == 0)
-            add_token(&head, &curr, tokens[i], TOKEN_RPAREN);
-        else
-            add_token(&head, &curr, tokens[i], TOKEN_WORD);  // Key change here
-        i++;
+    t_token *head = NULL;
+    t_token *curr = NULL;
+    int i = 0;
+    
+    while (input[i])
+    {
+        if (isspace(input[i]))
+        {
+            i++;
+            continue;
+        }
+        
+        if (input[i] == '|' || input[i] == '&' || 
+            input[i] == '<' || input[i] == '>' || 
+            input[i] == '(' || input[i] == ')')
+        {
+            handle_operator(&head, &curr, input, &i);
+            continue;
+        }
+        
+        if (input[i] == '"' || input[i] == '\'')
+        {
+            handle_quotes(&head, &curr, input, &i);
+            continue;
+        }
+        
+        handle_word(&head, &curr, input, &i);
     }
-    ft_free_split(tokens);
+    
     return head;
 }
 
-/* Redirection Parser */
+void handle_operator(t_token **head, t_token **curr, char *input, int *i)
+{
+    if (input[*i] == '&' && input[*i + 1] == '&')
+    {
+        add_token(head, curr, "&&", TOKEN_AND);
+        (*i) += 2;
+    }
+    else if (input[*i] == '|' && input[*i + 1] == '|')
+    {
+        add_token(head, curr, "||", TOKEN_OR);
+        (*i) += 2;
+    }
+    else if (input[*i] == '>' && input[*i + 1] == '>')
+    {
+        add_token(head, curr, ">>", TOKEN_APPEND);
+        (*i) += 2;
+    }
+    else if (input[*i] == '<' && input[*i + 1] == '<')
+    {
+        add_token(head, curr, "<<", TOKEN_HEREDOC);
+        (*i) += 2;
+    }
+    else if (input[*i] == '|')
+    {
+        add_token(head, curr, "|", TOKEN_PIPE);
+        (*i)++;
+    }
+    else if (input[*i] == '>')
+    {
+        add_token(head, curr, ">", TOKEN_REDIR_OUT);
+        (*i)++;
+    }
+    else if (input[*i] == '<')
+    {
+        add_token(head, curr, "<", TOKEN_REDIR_IN);
+        (*i)++;
+    }
+    else if (input[*i] == '(')
+    {
+        add_token(head, curr, "(", TOKEN_LPAREN);
+        (*i)++;
+    }
+    else if (input[*i] == ')')
+    {
+        add_token(head, curr, ")", TOKEN_RPAREN);
+        (*i)++;
+    }
+}
+
+void handle_quotes(t_token **head, t_token **curr, char *input, int *i)
+{
+    char quote = input[*i];
+    int start = ++(*i);
+    
+    while (input[*i] && input[*i] != quote)
+    {
+        if (quote == '"' && input[*i] == '\\' && input[*i + 1])
+            (*i)++;
+        (*i)++;
+    }
+    
+    int len = *i - start;
+    char *content = ft_substr(input, start, len);
+    add_token(head, curr, content, TOKEN_WORD);
+    free(content);
+    
+    if (input[*i])
+        (*i)++;
+}
+
+void handle_word(t_token **head, t_token **curr, char *input, int *i)
+{
+    int start = *i;
+    
+    while (input[*i] && !isspace(input[*i]) && 
+           input[*i] != '|' && input[*i] != '&' && 
+           input[*i] != '<' && input[*i] != '>' && 
+           input[*i] != '(' && input[*i] != ')' && 
+           input[*i] != '\'' && input[*i] != '"')
+    {
+        if (input[*i] == '\\' && input[*i + 1])
+            (*i) += 2;
+        else
+            (*i)++;
+    }
+    
+    int len = *i - start;
+    char *word = ft_substr(input, start, len);
+    add_token(head, curr, word, TOKEN_WORD);
+    free(word);
+}
+
+void print_tokens(t_token *tokens)
+{
+    t_token *current = tokens;
+    printf("--- Token List ---\n");
+    while (current)
+    {
+        printf("Type: %d, Value: '%s'\n", current->type, current->value);
+        current = current->next;
+    }
+    printf("-----------------\n");
+}
+
+int check_syntax_errors(t_token *tokens)
+{
+    t_token *current = tokens;
+    int paren_count = 0;
+    
+    while (current)
+    {
+        if (current->type == TOKEN_LPAREN)
+            paren_count++;
+        else if (current->type == TOKEN_RPAREN)
+            paren_count--;
+        
+        if (paren_count < 0)
+        {
+            flag_error(")");
+            return 1;
+        }
+        
+        if (!current->next && 
+            (current->type == TOKEN_PIPE || 
+             current->type == TOKEN_AND || 
+             current->type == TOKEN_OR || 
+             current->type == TOKEN_REDIR_IN || 
+             current->type == TOKEN_REDIR_OUT ||
+             current->type == TOKEN_APPEND ||
+             current->type == TOKEN_HEREDOC))
+        {
+            flag_error("newline");
+            return 1;
+        }
+        
+        current = current->next;
+    }
+    
+    if (paren_count > 0)
+    {
+        flag_error("(");
+        return 1;
+    }
+    
+    return 0;
+}
+
 static void parse_redirection(t_parser *parser, t_command *cmd)
 {
-    t_redir *new = malloc(sizeof(t_redir));
+    t_redir *new;
+    
+    new = malloc(sizeof(t_redir));
+    if (!new)
+        return;
     new->type = parser->current->type;
     parser->current = parser->current->next;
-
-    if (!parser->current || parser->current->type != TOKEN_WORD) {
+    if (!parser->current || parser->current->type != TOKEN_WORD)
+    {
         ft_putstr_fd("minishell: syntax error near unexpected token\n", 2);
         free(new);
         return;
     }
-
-    new->file = ft_strdup(parser->current->value);
+    new->file = ft_strdup(parser->current->value);    
     new->next = cmd->redirs;
-    cmd->redirs = new;
+    cmd->redirs = new;    
     parser->current = parser->current->next;
 }
 
@@ -136,13 +282,15 @@ static void parse_command(t_parser *parser, t_command *cmd)
     
     while (parser->current && 
           (parser->current->type == TOKEN_WORD ||
-           parser->current->type >= TOKEN_REDIR_IN)) {
-        
-        if (parser->current->type != TOKEN_WORD) {
+           parser->current->type >= TOKEN_REDIR_IN))
+    {
+        if (parser->current->type != TOKEN_WORD)
+        {
             parse_redirection(parser, cmd);
             continue;
         }
-        if (arg_count >= capacity-1) {
+        if (arg_count >= capacity-1)
+        {
             capacity *= 2;
             cmd->args = realloc(cmd->args, capacity * sizeof(char *));
         }
@@ -151,99 +299,3 @@ static void parse_command(t_parser *parser, t_command *cmd)
     }
     cmd->args[arg_count] = NULL;
 }
-
-
-
-// int main()
-// {
-//     char *input = "ls -la > ";
-//     t_token *tokens = tokenize(input);
-//     check_syntax_errors(tokens);
-//     t_parser parser = {tokens};
-//     t_ast *ast = parse(&parser);
-    
-//     print_ast(ast, 0);
-//     // cleaner(); // to do!!    
-//     return 0;
-// }
-// void print_tokens(t_token *tokens)
-// {
-//     t_token *current = tokens;
-//     while (current) {
-//         printf("Type: %d, Value: %s\n", current->type, current->value);
-//         current = current->next;
-//     }
-//     printf("\n");
-// }
-
-/* Parse pipe sequences */
-// t_ast *parse_pipeline(t_token **tokens)
-// {
-//     t_ast *left;
-//     t_ast *node;
-    
-//     left = parse_command(tokens);
-    
-//     if (*tokens != NULL && (*tokens)->type == NODE_PIPE)
-//     {
-//         *tokens = (*tokens)->next; // Consume the pipe
-        
-//         node = create_node(NODE_PIPE);
-//         node->left = left;
-//         node->right = parse_pipeline(tokens);
-//         return node;
-//     }
-    
-//     return left;
-// }
-
-/* Parse simple commands */
-// t_ast *parse_command(t_token **tokens)
-// {
-//     t_ast *node;
-//     int arg_count;
-//     t_token *current;
-//     int i;
-    
-//     if (*tokens == NULL || (*tokens)->type != NODE_COMMAND)
-//         return NULL;
-        
-//     node = create_node(NODE_COMMAND);
-    
-//     // Count args
-//     arg_count = 0;
-//     current = *tokens;
-//     while (current != NULL && current->type == NODE_COMMAND)
-//     {
-//         arg_count++;
-//         current = current->next;
-//     }
-    
-//     // Allocate args array
-//     node->args = malloc(sizeof(char*) * (arg_count + 1));
-//     if (!node->args)
-//         return NULL;
-    
-//     // Fill args array
-//     i = 0;
-//     while (i < arg_count)
-//     {
-//         node->args[i] = ft_strdup((*tokens)->value);
-//         *tokens = (*tokens)->next;
-//         i++;
-//     }
-//     node->args[i] = NULL;
-    
-//     return node;
-// }
-
-
-// void print_tokens(t_token *tokens)
-// {
-//     t_token *current = tokens;
-//     while (current) {
-//         printf("Type: %d, Value: %s\n", current->type, current->value);
-//         current = current->next;
-//     }
-//     printf("\n");
-// }
